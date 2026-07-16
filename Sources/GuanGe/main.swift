b813f551b0f26bf3927bb1dbb76f042f05d21908
wangyingsheng201-prog <wangyingsheng201@gmail.com>
2026-07-16 22:43:54 +0800

    fix: 在主线程启动 macOS 应用

--- Sources/GuanGe/main.swift
+++ Sources/GuanGe/main.swift
@@ -1,5 +1,13 @@
 import AppKit
 
+MainActor.assumeIsolated {
+    let application = NSApplication.shared
+    let delegate = AppDelegate()
+    application.delegate = delegate
+    application.run()
+}
+import AppKit
+
 let application = NSApplication.shared
 let delegate = AppDelegate()
 application.delegate = delegate
